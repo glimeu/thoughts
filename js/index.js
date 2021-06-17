2a0c13c7aa984044f1b5b8a63c28d879e955c66b
Miguel Ângelo <miguelcg958@gmail.com>
2021-06-17 01:04:18 -0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", onReady);
+
+const months = Array.from({ length: 12 }, (_, i) =>
+  new Date(0, i)
+    .toLocaleDateString("pt-BR", { month: "long" })
+    .split("")
+    .map((char, charI) => (charI === 0 ? char.toUpperCase() : char))
+    .join("")
+);
+/* const logs = [
+  {
+    date: "16-06-2021",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nam animi neque laborum perferendis aliquid, labore a ratione voluptatibus ut modi nesciunt ea totam. Quisquam voluptatibus at mollitia nisi amet!",
+  },
+  {
+    date: "15-06-2021",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nam animi neque laborum perferendis aliquid, labore a ratione voluptatibus ut modi nesciunt ea totam. Quisquam voluptatibus at mollitia nisi amet!",
+  },
+  {
+    date: "14-06-2021",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nam animi neque laborum perferendis aliquid, labore a ratione voluptatibus ut modi nesciunt ea totam. Quisquam voluptatibus at mollitia nisi amet!",
+  },
+  {
+    date: "13-06-2021",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nam animi neque laborum perferendis aliquid, labore a ratione voluptatibus ut modi nesciunt ea totam. Quisquam voluptatibus at mollitia nisi amet!",
+  },
+]; */
+
+async function onReady() {
+  const mainElem = document.querySelector("body main");
+  const logs = await fetch("/data.json").then((res) => res.json());
+
+  logs.forEach((log) => {
+    const date = log.date.split("-").map((str) => +str);
+    const logElem = createElement(
+      `${date[0]} de ${months[date[1]]} de ${date[2]}`,
+      log.content
+    );
+
+    mainElem.innerHTML = mainElem.innerHTML + logElem;
+  });
+}
+
+/**
+ * @param {string} date
+ * @param {string} content
+ * @returns
+ */
+function createElement(date, content) {
+  return `
+  <section class="elevation-1">
+    <strong class="title-heavy">${date}</strong>
+    <p class="body-light">${content.replaceAll("\n", "<br />")}</p>
+  </section>
+  `;
+}
